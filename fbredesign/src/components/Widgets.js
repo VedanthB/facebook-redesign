dd4234e7a876ea3b7075019a7904a347a4132dae
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-05-03 06:53:13 +0530

    added iframe

---
+++ fbredesign/src/components/Widgets.js
@@ -0,0 +1,18 @@
+function Widgets() {
+    return (
+        <div className='widgets'>
+            <iframe 
+             src='https://www.linkedin.com/embed/feed/update/urn:li:share:6791127173022543872'
+             width='340'
+             height='800'
+             style={{border: 'none', overflow: 'hidden'}}
+             scrolling='yes'
+             frameborder='0'
+             allowTransprancy='true'
+             aloow='encrypted-media'
+            ></iframe>
+        </div>
+    )
+}
+
+export default Widgets
